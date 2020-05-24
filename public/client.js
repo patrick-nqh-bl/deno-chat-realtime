e61f133774a120f4b8d64851bb0690827e2f0be5
patrick <patricknguyen2047@gmail.com>
2020-05-24 12:25:10 +0700

    get search from 'window.location.search'

---
+++ public/client.js
@@ -0,0 +1,35 @@
+let ws;
+
+window.addEventListener("DOMContentLoaded", () => {
+  ws = new WebSocket(`ws://localhost:3000/ws`);
+  ws.addEventListener("open", onConnectionOpen);
+  ws.addEventListener("message", onMessageReceived);
+});
+
+function onConnectionOpen() {
+  console.log("Connection opened");
+  const queryParams = getQueryParams();
+  console.log(queryParams);
+  const event = {
+    event: "join",
+    groupName: queryParams.group,
+    name: queryParams.name,
+  };
+  ws.send(event);
+}
+
+function onMessageReceived() {
+  // Message received
+}
+
+function getQueryParams() {
+  const search = window.location.search.substring(1);
+  const pairs = search.split("&");
+  const params = {};
+  for (const pair of pairs) {
+    const parts = pair.split("=");
+    params[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+  }
+
+  return params;
+}
